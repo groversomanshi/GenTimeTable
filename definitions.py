d97e4c35d8a8cb7fe366a6790c6aad5045e68da5
groversomanshi <groversomanshi@gmail.com>
2025-04-15 12:01:12 -0500

    changed order of arguments in schoolStructure for consistency

--- definitions.py
+++ definitions.py
@@ -12,17 +12,17 @@ class Days(Enum):
 
 #class containing data about the school's week structure
 class schoolStructure:
-    def __init__(self, weekTotal, numDays):
-        self.__weekTotal = int(weekTotal) #total periods in a week
+    def __init__(self, numDays, weekTotal):
         self.__numDays = int(numDays) #workings days per week
+        self.__weekTotal = int(weekTotal) #total periods in a week
         self.__dayTotal = self.__weekTotal // self.__numDays #number of periods in a day
 
     #getter functions
-    def getWeekTotal(self):
-        return self.__weekTotal
-    
     def getNumDays(self):
         return self.__numDays
+
+    def getWeekTotal(self):
+        return self.__weekTotal
     
     def getDayTotal(self):
         return self.__dayTotal
