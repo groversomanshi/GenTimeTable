dcd36166344c3e59109287cf623c976fed07375c
groversomanshi <groversomanshi@gmail.com>
2025-04-15 12:02:19 -0500

    added schoolstructure object data to its table

--- dataCollection.py
+++ dataCollection.py
@@ -14,9 +14,21 @@ file = fileName + '.sqlite'
 conn = sqlite3.connect(file)
 cur = conn.cursor()
 cur.executescript('''
+                  CREATE TABLE IF NOT EXISTS SchoolStructure (numDays INTEGER, weekTotal INTEGER, dayTotal INTEGER);
                   CREATE TABLE IF NOT EXISTS Teachers (id TEXT NOT NULL PRIMARY KEY UNIQUE, name TEXT, weekMax INTEGER, weekMin INTEGER, dayMax INTEGER, dayMin INTEGER);
                   CREATE TABLE IF NOT EXISTS Subjects (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, subjectName TEXT UNIQUE);
-                  CREATE TABLE IF NOT EXISTS Classes (id INNTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, grade INTEGER, section TEXT);
+                  CREATE TABLE IF NOT EXISTS Classes (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, grade INTEGER, section TEXT);
                   CREATE TABLE IF NOT EXISTS TeacherSubjects (teacherId TEXT, subjectId INTEGER, PRIMARY KEY(teacherId, dubjectId));
                   CREATE TABLE IF NOT EXISTS SubjectClasses (classId INTEGER, subjectId INTEGER, teacherId TEXT, weekPeriods INTEGER, dayPeriods INTEGERS, PRIMARY KEY(classId, subjectId, teacherId));
                   ''')
+
+#adding new School Structure as an object of class schoolStructure and in the database table
+if input('Type YES to fill School Structure: ').strip() == 'YES'.upper():
+    cur.execute('SELECT COUNT(*) FROM SchoolStructure')
+    if cur.fetchone()[0] > 0:
+        print('Error. SchoolStructure already exists.')
+    else:
+        print('Enter numDays, weekTotal')
+        numDays, weekTotal = map(int, input('E.g. 5, 40: ').split(','))
+        structure = schoolStructure(numDays, weekTotal)
+        cur.execute('INSERT INTO SchoolStructure (numDays, weekTotal, dayTotal) VALUES (?, ?, ?)', (structure.getNumDays(), structure.getWeekTotal(), structure.getNumDays()))
